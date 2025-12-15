10c2b3b262715c9a34944518ca690c6d66ba8628
Brian Lindquist <brian@brianlindquist.io>
2025-12-15 16:32:42 -0600

    update header logo and hero section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,9 +5,9 @@ export const Hero = () => {
     <section className="flex items-center justify-center min-h-[80vh] sm:min-h-[85vh] lg:min-h-[90vh] pt-24 sm:pt-28 lg:pt-32 px-4 sm:px-6 lg:px-8 w-full">
       <div className="flex flex-col lg:flex-row items-center gap-8 lg:gap-12 max-w-6xl w-full">
         <div className="flex-shrink-0">
-          <img 
-            src={profile} 
-            alt="Profile" 
+          <img
+            src={profile}
+            alt="Profile"
             className="w-48 h-48 sm:w-56 sm:h-56 lg:w-64 lg:h-64 rounded-full object-cover shadow-xl ring-4 ring-white"
           />
         </div>
