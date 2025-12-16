c1c18daab20b532bda9d27c5e4d81134cfd6db46
Brian Lindquist <brian@brianlindquist.io>
2025-12-16 11:01:40 -0600

    add about me section

--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -1,6 +1,6 @@
 import { Contact } from "@/components/Contact";
 import { Hero } from "@/components/Hero";
-import { Profile } from "@/components/Profile";
+import { AboutMe } from "@/components/AboutMe";
 
 export const ProfilePage = () => {
   return (
@@ -8,7 +8,7 @@ export const ProfilePage = () => {
       <Hero />
 
       <div className="flex flex-col sm:flex-row gap-4 px-4 sm:px-6 lg:px-8 pb-8 max-w-6xl mx-auto">
-        <Profile />
+        <AboutMe />
         <Contact />
       </div>
     </>
